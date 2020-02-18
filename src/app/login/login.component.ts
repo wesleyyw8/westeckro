e550739f58cacd3ec30eddbc66ed8753bdb475ca
wesley <wesleyyw8@gmail.com>
2020-02-18 12:52:48 +0000

    validation done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+import { User } from './user';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent implements OnInit {
-  
-  isPassword: boolean = true;
+  loginForm: FormGroup;
+  isPasswordVisible: boolean = true;
+  user = new User();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {}
-
-  showPassword(){
-    this.isPassword = false;
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, this.customRegexValidator(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/i)]],
+      password: ['', [Validators.required,
+        this.customRegexValidator(/(?=^.{8,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?\/&gt;.&lt;,])(?!.*\s).*$/)]],
+    });
   }
- 
-  hidePassword(){
-    this.isPassword = true;
+
+  customRegexValidator(nameRe: RegExp): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      const customRegex = nameRe.test(control.value);
+      return customRegex ? null : {customRegex: {value: control.value}};
+    };
+  }
+
+  showPassword() {
+    this.isPasswordVisible = false;
+  }
+
+  hidePassword() {
+    this.isPasswordVisible = true;
   }
 }
---
+++ src/app/login/user.ts
@@ -0,0 +1,7 @@
+export class User {
+
+  constructor(
+    public firstName = '',
+    public password = ''
+    ) { }
+}
